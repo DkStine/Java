c1c06f0cd597e511998e3d4c8f1a06afa379f27e
DkStine <dkctcak@gmail.com>
2023-09-07 22:01:38 +0530

    IntroStrings

---
+++ Chapter3/StringsIntro.java
@@ -0,0 +1,8 @@
+package Chapter3;
+
+public class StringsIntro {
+    public static void main(String[] args) {
+        String string = new String("Deepak Kumar");
+        System.out.println(string);
+    }
+}
