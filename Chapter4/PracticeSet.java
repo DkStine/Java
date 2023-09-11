34881b8c101573b43ba63a5d56b2fffe13bf72bd
DkStine <dkctcak@gmail.com>
2023-09-11 19:12:20 +0530

    PracticeSet4

---
+++ Chapter4/PracticeSet.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class PracticeSet {
+    public static void main(String[] args) {
+        // Q1
+        // Scanner in = new Scanner(System.in);
+
+        // int Phy, Chem, Maths;
+
+        // System.out.println("Enter marks in Physics: ");
+        // Phy = in.nextInt();
+        // System.out.println("Enter marks in Chemistry: ");
+        // Chem = in.nextInt();
+        // System.out.println("Enter marks in Maths: ");
+        // Maths = in.nextInt();
+
+        // in.close();
+
+        // if((Phy + Chem + Maths)/3 >= 40){
+        //     if(Phy >= 33 && Chem >= 33 && Maths >= 33){
+        //         System.out.println("You have scored " + (Phy + Chem + Maths) + " and passed");
+        //     }
+        //     else{
+        //         System.out.println("You have scored " + (Phy + Chem + Maths) + " but failed");
+        //     }
+        // }
+        // else{
+        //     System.out.println("You have scored " + (Phy + Chem + Maths) + " but failed");
+        // }
+
+        // Q4
+        // Scanner in = new Scanner(System.in);
+
+        // byte wDay = in.nextByte();
+        // in.close();   --> Easy question
+
+
+
+
+    }
+}
