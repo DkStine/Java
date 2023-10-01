8db26f36c5a36ce0e1498d973ed64cb84ab95258
DkStine <dkctcak@gmail.com>
2023-10-01 15:46:24 +0530

    MiscellaneousRepo init

--- Chapter4/PracticeSet.java
+++ Chapter4/PracticeSet.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class PracticeSet {
     public static void main(String[] args) {
---
+++ Miscellaneous/ScientificCalculator.java
@@ -0,0 +1,8 @@
+package Miscellaneous;
+
+public class ScientificCalculator {
+    public static void main(String[] args) {
+        System.out.println("Tumhare teacher ne tumhe kaunsa calculator kharidne ko bola hai?");
+    }
+    
+}
