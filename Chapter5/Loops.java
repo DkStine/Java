bf2efd5f7a1249205a2c6e2159ca4b4066ff17ca
DkStine <dkctcak@gmail.com>
2023-10-07 12:30:11 +0530

    random

--- Chapter5/Loops.java
+++ Chapter5/Loops.java
@@ -20,16 +20,21 @@ public class Loops {
         // } while (i <= 10);
 
         // --> for loop
-        int n;
-        Scanner in = new Scanner(System.in);
+        
+        // for (int i = 0; i <= n; i++){
+            //     System.out.println(2*i + 1);
+            //     }
+            
+            int n;
+            Scanner in = new Scanner(System.in);
+            
+            System.out.println("Start");
+            n = in.nextInt();
+            in.close();
 
-        n = in.nextInt();
-        in.close();
-
-        for (int i = 1; i <= n; i++){
-            if (i % 2 != 0) {
-                System.out.println(i);
+            for(int i = n; i != 0; i--){
+                System.out.print(i + " ");
             }
+            
         }
     }
-}
