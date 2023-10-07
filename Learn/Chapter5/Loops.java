29dc027c338cb98923489ffbf5ac5fadd8bc87ff
DkStine <dkctcak@gmail.com>
2023-10-07 19:19:21 +0530

    Arrays

--- Learn/Chapter5/Loops.java
+++ Learn/Chapter5/Loops.java
@@ -1,5 +1,5 @@
 package Chapter5;
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Loops {
     public static void main(String[] args) {
---
+++ Learn/Chapter6/Arrays.java
@@ -0,0 +1,14 @@
+package Chapter6;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int [] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
