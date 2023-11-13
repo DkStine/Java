f45aee14c0f4a352707e7ac9ecf771a677b6db33
Deepak_Kumar <dkctcak@gmail.com>
2023-11-13 18:10:59 +0530

    Shapes

---
+++ Learn/OOPs/Shapes.java
@@ -0,0 +1,12 @@
+// Rectangle, Square, circle, cylinder, square
+
+interface Dimension {
+    void area();
+    void volume();
+}
+
+public class Shapes {
+    public static void main(String[] args) {
+        
+    }
+}
