751de68f01f3634fa0871d89dc4155522a746049
Deepak_Kumar <dkctcak@gmail.com>
2023-11-18 18:45:08 +0530

    RunnableInterface

--- Learn/OOPs/Threading.java
+++ Learn/OOPs/Threading.java
@@ -1,3 +1,4 @@
+// Extend Thread class
 class Cooking extends Thread {
     @Override
     public void run() {
@@ -23,8 +24,31 @@ class Chatting extends Thread {
     }
 }
 
+// Create Runnable bullet(object) -> implement Runnable interface
+class Cooking1 implements Runnable {
+    @Override
+    public void run() {
+        int i = 1;
+
+        while (i < 100) {
+            System.out.println("Cooking...");
+        }
+    }
+}
+class Chatting1 implements Runnable {
+    @Override
+    public void run() {
+        int i = 1;
+
+        while (i < 100) {
+            System.out.println("Chatting...");
+        }
+    }
+}
+
 public class Threading {
     public static void main(String[] args) {
+        /* --> Extending Thread class
         Cooking cook = new Cooking();
         Coding code = new Coding();
         Chatting chat = new Chatting();
@@ -32,5 +56,16 @@ public class Threading {
         cook.start();
         code.start();
         chat.start();
+        */
+        
+        /* --> Implementing Runnable Interface*/
+        Cooking1 bullet1 = new Cooking1();
+        Thread gun1 = new Thread(bullet1);
+        Chatting1 bullet2 = new Chatting1();
+        Thread gun2 = new Thread(bullet2);
+
+        gun1.start();
+        gun2.start();
+
     }
 }
