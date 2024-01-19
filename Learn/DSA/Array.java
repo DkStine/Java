9982a4f8c7bb9a01c96fb6f2644a2265ed3bdd7e
Deepak_Kumar <dkctcak@gmail.com>
2024-01-19 23:12:37 +0530

    Arrays

--- Learn/DSA/Array.java
+++ Learn/DSA/Array.java
@@ -14,6 +14,40 @@ public class Array {
         int[] finalArr = {arr[0], arr[arr.length - 1]};
         return finalArr;
     }
+    static int tripletSum(int[] arr, int target) {
+        int tripletCount = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+                    int sum = arr[i] + arr[j] + arr[k];
+                    if (sum == target) {
+                        tripletCount++;
+                    }
+                    sum = 0;
+                }
+            }
+        }
+        return tripletCount;
+
+    }
+    static int uniqueArrayElement(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    arr[i] = arr[j] = -1;
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int k : arr) {
+            if (k != -1) {
+                ans = k;
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         // Arrays_1
         // Q1
@@ -81,7 +115,60 @@ public class Array {
         // int[] ansArr = smallestAndLargestElement(arr);
 
         // printArray(ansArr);
+        // pairSum
+        /* 
+        int[] arr = {4, 6, 3, 5, 8, 2};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input no.: ");
+        int x = sc.nextInt();
+        sc.close();
+        int ansCount = 0;
+        
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int sum = arr[i] + arr[j];
+                if (sum == x) {
+                    ansCount++;
+                }
+                sum = 0;
+            }
+        }
+        
+        System.out.println(ansCount);
+        */
+        
+        // Triplet Sum
+        /* 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter sum: ");
+        int x = sc.nextInt();
+        int[] arr = new int[n];
 
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        
+        System.out.println(tripletSum(arr, x));
+        */
+        
+        // Array Manipulation
+        /* 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
 
+        System.out.println(uniqueArrayElement(arr));
+        */
+
+        
     }
 }
